3e4529d15a0de685705345f1473a33e7ff0ac750
nandikaL <ox24006@bristol.ac.uk>
2024-11-15 17:58:57 +0000

    Added in classes to main as running into import error

--- Main Start.py
+++ Main Start.py
@@ -1,8 +1,78 @@
-from FishClass import Fish
-from WarehouseClass import Warehouse
-from TechicianClass import Techician
-from VendorClass import Vendor
-from HatcheryClass import Hatchery
+#All classes placed here as importing is not working
+
+######Fish Class
+
+class Fish: 
+    
+    def __init__(self,species,fertilizer,feed,salt,maintaince_time,demand,price):
+        self.species = species
+        self.fertilizer = fertilizer
+        self.feed = feed
+        self.salt = salt
+        self.maintaince_time = maintaince_time
+        self.demand = demand
+        self.price = price
+
+    def description(self):
+        print(f"{self.species}: Fertilizer Use:{self.fertilizer}ml \nFeed use: {self.feed}kg \nSalt Use:{self.salt}kg \nMaintainence Time: {self.maintaince_time}")
+
+    def Price(self):
+        print(f"{self.species}: Price:${self.price}")
+
+Fin = Fish('Clef Fins',100.0,12,2,2.0,25,250)
+Snapper = Fish('Timpani Snapper', 50.0, 9, 2, 1.0, 10, 350)
+Brim = Fish('Andalusian Brim', 90.0, 6, 2, 0.5, 15, 250)
+Cod = Fish('Plagal Cod', 100.0, 10, 2, 2.0, 20, 400)
+Flounder = Fish('Fugue Flounder', 200.0, 12, 2, 2.5, 30, 550)
+Bass = Fish('Modal Bass', 300.0, 12, 6, 3.0, 50, 500) 
+
+###### Warehouse Class
+
+class Warehouse:
+    def __init__(self, warehouse, fertilizer_capacity,feed_capacity,salt_capacity):
+        self.warehouse = warehouse
+        self.fertilizer_capacity = fertilizer_capacity
+        self.feed_capacity = feed_capacity
+        self.salt_capacity = salt_capacity
+        self.fertilizer_depreciation = 0.4
+        self.feed_depreciation = 0.1
+        self.salt_depreciation = 0.0
+        self.fertilizer_warehouse = 0.1
+        self.feed_warehouse = 0.001
+        self.salt_warehouse = 0.001
+
+Main_Warehouse = Warehouse('Main',20,400,200)
+Aux_Warehouse = Warehouse('Aux',10,200,100)
+print(Main_Warehouse.__dict__)
+print(Aux_Warehouse.__dict__)
+
+#depreciation = cost x remainder
+
+##### Technician Class
+class Techician:
+    def __init__(self,name):
+        self.name = name
+
+###### Vendor Class
+
+class Vendor:
+    def __init__(self,name,fertilizer_cost,feed_cost,salt_cost):
+        self.name = name
+        self.fertilizer_cost = fertilizer_cost
+        self.feed_cost = feed_cost
+        self.salt_cost = salt_cost
+    
+    def display(self):
+        print(f"{self.name}: Fertilizer Cost:£{self.fertilizer_cost}/litre \nFeed Cost:£{self.feed_cost}/g \nSalt Cost{self.salt_cost}/g")
+
+##### Hatchery Class
+
+class Hatchery:
+    def __init__(self,fish_quantity,supplies,cash,techs):
+        self.fish_quantity = fish_quantity
+        self.supplies = supplies
+        self.cash = cash
+        self.techs = techs
 
 #Very rough plan. 
 #Inputs
