7ea08d8126983a357e89ad4a053132e71d990bf5
nandikaL <ox24006@bristol.ac.uk>
2024-11-18 13:24:58 +0000

    Spelling error on fish class, further refine quaterclass stock depletion

--- FishClass.py
+++ FishClass.py
@@ -1,16 +1,16 @@
 class Fish: 
     
-    def __init__(self,species,fertilizer,feed,salt,maintaince_time,demand,price):
+    def __init__(self,species,fertilizer,feed,salt,maintenance_time,demand,price):
         self.species = species
         self.fertilizer = fertilizer
         self.feed = feed
         self.salt = salt
-        self.maintaince_time = maintaince_time
+        self.maintenance_time = maintenance_time
         self.demand = demand
         self.price = price
 
     def description(self):
-        print(f"{self.species}: Fertilizer Use:{self.fertilizer}ml \nFeed use: {self.feed}kg \nSalt Use:{self.salt}kg \nMaintainence Time: {self.maintaince_time}")
+        print(f"{self.species}: Fertilizer Use:{self.fertilizer}ml \nFeed use: {self.feed}kg \nSalt Use:{self.salt}kg \nMaintainence Time: {self.maintenance_time}")
 
     def Price(self):
         print(f"{self.species}: Price:${self.price}")
--- QuarterClass.py
+++ QuarterClass.py
@@ -8,46 +8,69 @@ from VendorClass import Vendor
 from HatcheryClass import Hatchery
 
 class Quarter:
-    current_stock_dict = {'maint_time':0, 'fert':0, 'feed':0, 'salt':0, 'cash':0}
+    current_stock_dict = {'maint_time':0, 'fert':0, 'feed':0, 'salt':0, 'cash':10000}
 
     def Current_stocks():
         Quarter.Current_stock_dict['maint_time']=len(Hatchery.current_techs)*45 #45 days of work per person
 
         #Adding sum of items in the warehouse 
-        Quarter.current_stock_dict['feed'] = sum(Warehouse.Fert)
+        Quarter.current_stock_dict['fert'] = sum(Warehouse.Fert)
         Quarter.current_stock_dict['feed'] = sum(Warehouse.Feed)
-        Quarter.current_stock_dict['feed'] = sum(Warehouse.Salt)
+        Quarter.current_stock_dict['salt'] = sum(Warehouse.Salt)
+
+        #does anything happen with cash here?
     
 
     def deplete_stocks(fishy):
         
         for fish in fishy: 
 
-            demand = Fish.fertilizer.demand
+            demand = Fish.demand
             
             fert_need = Fish.fertilizer * demand
             feed_need = Fish.feed * demand
             salt_need = Fish.salt * demand
+            maint_need = Fish.maintenance_time * demand
 
             if Quarter.current_stock_dict['fert'] >= fert_need:
-                new_fert_value = Quarter.current_stock_dict['fert'] - fert_need
-                Quarter.current_stock_dict['fert'].append(new_fert_value)
                 print(f"{fish.species}: {fert_need}ml fertilizer used.")
             else:
                 print(f"Not enough fertilizer for {fish.species}. {fert_need}ml needed, only {Quarter.current_stock_dict['fert']}ml available.")
+                continue
 
             if Quarter.current_stock_dict['feed'] >= feed_need:
-                new_feed_value=Quarter.current_stock_dict['feed'] - feed_need
-                Quarter.current_stock_dict['feed'].append(new_feed_value)
                 print(f"{fish.species}: {feed_need}kg feed used.")
             else:
                 print(f"Not enough feed for {fish.species}. {feed_need}kg needed, only {Quarter.current_stock_dict['feed']}kg available.")
+                continue
 
             if Quarter.current_stock_dict['salt'] >= salt_need:
-                new_salt_value= Quarter.current_stock_dict['salt'] - salt_need
                 print(f"{fish.species}: {salt_need}kg salt used.")
             else:
                 print(f"Not enough salt for {fish.species}. {salt_need}kg needed, only {Quarter.current_stock_dict['salt']}kg available.")
+                continue
+            #####Maintenence 
+            if Quarter.current_stock_dict['maint_time']>=maint_need:
+                print(f"{maint_need} days of work taken")
+            else:
+                print(f"Not enough labour for {fish.species}. {maint_need} required, only {Quarter.current_stock_dict['maint_time']} available")
+                continue
+
+            new_fert_value = Quarter.current_stock_dict['fert'] - fert_need
+            Quarter.current_stock_dict['fert'].append(new_fert_value)
+            new_feed_value=Quarter.current_stock_dict['feed'] - feed_need
+            Quarter.current_stock_dict['feed'].append(new_feed_value)
+            new_salt_value= Quarter.current_stock_dict['salt'] - salt_need
+            Quarter.current_stock_dict['salt'].append(new_salt_value)
+            new_maint_value = Quarter.current_stock_dict['maint_time']-maint_need
+            Quarter.current_stock_dict['maint_time'].append(new_maint_value)
+
+            ##add money
+            fish_rev = Fish.Price * demand 
+            add_rev = Quarter.current_stock_dict['cash']+fish_rev
+            Quarter.current_stock_dict['cash']+fish_rev
+
+
 
     #ok so: fish have fert, feed, salt
     #1st type of fish: input number being sold:
@@ -55,8 +78,12 @@ class Quarter:
     #if enough fert in warehouses, move to next item, feed. 
     #if enough feed in warehouses, move to next item 
     #if enough salt in warehouses, move to next item
-        pass
+    #must meet all variables before continue
+    #then  do labour
+    #the append everything
+    #loopy loop
     
+
     def payments():
         standard = 1500
         tech_payments = len(Hatchery.current_techs)*4500 #500 mulitpled by 9 weeks 
